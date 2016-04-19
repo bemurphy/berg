68d7cd76804f2a1b1c3512995b1d1571bc0efc93
Dylan Wolff <dylanwolff@gmail.com>
2016-04-19 09:21:57 +0700

    Add initial users edit form

---
+++ apps/admin/lib/admin/forms/users/edit_form.rb
@@ -0,0 +1,31 @@
+require "formalist"
+require "formalist/elements/standard"
+
+module Admin
+  module Forms
+    module Users
+      EditForm = Class.new(Formalist::Form) do
+        define do
+          section :user do
+            text_field :name,
+              label: "Name",
+              validation: {filled?: true}
+
+            text_field :email,
+              label: "Email",
+              validation: {filled?: true}
+
+            text_field :password,
+              label: "Password",
+              password: true,
+              hint: "Leave blank if you don't want to change it"
+
+            text_field :password_confirmation,
+              label: "Password confirmation",
+              password: true
+          end
+        end
+      end.new
+    end
+  end
+end
