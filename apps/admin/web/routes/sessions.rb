596f66a030e182cadcf6b9d3fae6c33d9cedc34b
Dylan Wolff <dylanwolff@gmail.com>
2016-04-20 11:44:00 +0700

    Add initial routes for Sessions and Users

---
+++ apps/admin/web/routes/sessions.rb
@@ -0,0 +1,30 @@
+class Admin::Application < Dry::Web::Application
+  route("sign-in") do |r|
+    r.get do
+      r.view("sign_in")
+    end
+
+    r.post do
+      r.resolve("admin.authentication.authenticate") do |authenticate|
+        authenticate.(r[:user]) do |m|
+          m.success do |user|
+            flash["notice"] = "You are now signed in"
+            session[:user_id] = user.id
+            r.redirect "/admin"
+          end
+
+          m.failure do |error|
+            flash["notice"] = t["errors.#{error}"]
+            r.redirect "/admin/sign-in"
+          end
+        end
+      end
+    end
+  end
+
+  route("sign-out") do |r|
+    flash["notice"] = "You are now signed out"
+    session[:user_id] = env["admin.current_user"] = nil
+    r.redirect "/admin/sign-in"
+  end
+end
--- apps/admin/web/routes/users.rb
+++ apps/admin/web/routes/users.rb
@@ -1,26 +1,68 @@
-module Admin
-  class Application < Dry::Web::Application
-    route "user" do |r|
+class Admin::Application < Dry::Web::Application
+  route("users") do |r|
+
+    r.authorize do
+      r.get("") do
+        r.view("users.index", page: r[:page] || 1)
+      end
+
+      r.get("new") do
+        r.view("users.new")
+      end
+
       r.post do
-        r.resolve "admin.operations.users.update_user" do |update_user|
-          update_user.(current_user.id, r[:user]) do |m|
+        r.resolve("admin.transactions.create_user") do |create_user|
+          create_user.(r[:user]) do |m|
             m.success do
-              flash[:notice] = "Your account has been updated."
-              r.redirect "/admin"
+              flash["notice"] = "User has been created"
+              r.redirect "/admin/users"
             end
 
-            m.failure do |errors|
-              r.resolve "admin.views.user.edit" do |view|
-                view.(scope: current_page, params: r[:user], errors: errors)
-              end
+            m.failure do |validation|
+              r.view("users.new", validation: validation)
             end
           end
         end
       end
 
-      r.is "edit" do
-        r.get do
-          r.view "user.edit"
+      r.on(":id") do |id|
+        r.get("edit") do
+          r.view("users.edit", id: id)
+        end
+
+        r.is do
+          r.resolve("admin.users.operations.update") do |update_user|
+            update_user.(id, r[:user]) do |m|
+              m.success do
+                flash["notice"] = "User has been updated"
+                r.redirect "/admin/users/#{id}/edit"
+              end
+
+              m.failure do |validation|
+                r.view("users.edit", user_validation: validation)
+              end
+            end
+          end
+        end
+
+        r.on("password") do
+          r.get do
+            r.view("users.password", id: id)
+          end
+          r.put do
+            r.resolve("admin.users.operations.change_password") do |change_password|
+              change_password.(id, r[:user]) do |m|
+                m.success do
+                  flash["notice"] = "Password has been changed"
+                  r.redirect "/admin/users/#{id}/password"
+                end
+
+                m.failure do |validation|
+                  r.view("users.password", pass_validation: validation)
+                end
+              end
+            end
+          end
         end
       end
     end
