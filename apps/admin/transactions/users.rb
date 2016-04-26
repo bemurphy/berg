7be13fca376ad1a3006102482d607b301123a1af
Dylan Wolff <dylanwolff@gmail.com>
2016-04-26 11:24:06 +0700

    Rename password request transaction

--- apps/admin/transactions/users.rb
+++ apps/admin/transactions/users.rb
@@ -6,7 +6,7 @@ Admin::Transactions.define do |t|
     enqueue :send_activation_email, with: "admin.users.operations.send_activation_email"
   end
 
-  t.define "admin.transactions.password_reset_request" do
+  t.define "admin.transactions.request_password_reset" do
     step :update_access_token, with: "admin.users.operations.update_access_token"
     enqueue :send_password_reset_email, with: "admin.users.operations.send_password_reset_email"
   end
