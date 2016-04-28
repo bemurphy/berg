d63afe29ac0a4081ac6f6332803416ce1163d6a9
Dylan Wolff <dylanwolff@gmail.com>
2016-04-28 15:49:45 +0700

    Resolve deprecation warning in Users validation form

--- apps/admin/lib/admin/users/validation/form.rb
+++ apps/admin/lib/admin/users/validation/form.rb
@@ -4,11 +4,11 @@ module Admin
   module Users
     module Validation
       Form = Berg::Validation.Form do
-        optional(:email).required
-        optional(:first_name).required
-        optional(:last_name).required
-        optional(:active).required(:bool?)
-        optional(:password).required(min_size?: 8)
+        optional(:email).filled
+        optional(:first_name).filled
+        optional(:last_name).filled
+        optional(:active).filled(:bool?)
+        optional(:password).filled(min_size?: 8)
       end
     end
   end
