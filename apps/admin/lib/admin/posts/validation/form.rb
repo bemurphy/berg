9a3cdad10fb0e0859fe2ceb29e23e48242e06e5c
Dylan Wolff <dylanwolff@gmail.com>
2016-05-04 18:53:29 +0700

    Remove slug field from Posts forms

--- apps/admin/lib/admin/posts/validation/form.rb
+++ apps/admin/lib/admin/posts/validation/form.rb
@@ -6,7 +6,6 @@ module Admin
       Form = Berg::Validation.Form do
         optional(:title).filled
         optional(:body).filled
-        optional(:slug).filled
         optional(:author_id).filled(:int?)
         optional(:status).filled(inclusion?: Entities::Post::Status.values)
       end
