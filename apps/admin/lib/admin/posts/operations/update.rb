a75207f9a05a936f2cef750eec46f51d33345aa5
Tim Riley <tim@openmonkey.com>
2016-05-09 13:12:39 +1000

    Massage attributes in 2 stages when updating posts
    1. Before sending to validation (so that an unchanged slug can be removed from the attributes)
    2. Before sending to DB update command (to add some extra attributes)
    
    This will allow slug validation to work as expected.
    
    Also remove automatic slugifying behaviour on post update. At this point, if a user is modifying a slug, it can be their responsibility to make sure it’s unique.

--- apps/admin/lib/admin/posts/operations/update.rb
+++ apps/admin/lib/admin/posts/operations/update.rb
@@ -9,17 +9,16 @@ module Admin
       class Update
         include Admin::Import(
           "admin.persistence.repositories.posts",
-          "admin.posts.slugify"
         )
 
         include Dry::ResultMatcher.for(:call)
 
         def call(slug, attributes)
-          validation = Validation::Form.(attributes)
+          validation = Validation::Form.(prepare_attributes_for_validation(slug, attributes))
 
           if validation.success?
-            posts.update_by_slug(slug, prepare_attributes(slug, validation.output))
-            Right(posts.by_slug(slug))
+            posts.update_by_slug(slug, prepare_attributes_for_update(validation.output))
+            Right(posts.by_slug(validation.output.fetch(:slug) { slug }))
           else
             Left(validation)
           end
@@ -27,20 +26,18 @@ module Admin
 
         private
 
-        def prepare_attributes(slug, attributes)
-          # Only update the slug if a slug is present in the attrs, and it's been modified
-          if attributes[:slug] && attributes[:slug] != slug
-            slug = slugify.(attributes[:slug], posts.method(:slug_exists?))
+        def prepare_attributes_for_validation(slug, attributes)
+          attributes.dup.tap do |attrs|
+            # Only keep the slug for validation/update if it has been modified
+            attrs.delete("slug") if (new_slug = attributes["slug"]) && new_slug == slug
           end
+        end
+
+        def prepare_attributes_for_update(attributes)
           if attributes[:status] == "published"
-            attributes.merge(
-              published_at: DateTime.now,
-              slug: slug
-            )
+            attributes.merge(published_at: Time.now)
           else
-            attributes.merge(
-              slug: slug
-            )
+            attributes
           end
         end
       end
