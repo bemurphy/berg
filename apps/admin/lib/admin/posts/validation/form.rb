4258d09677086ff35eaadbb2969117cf247f5bb4
Dylan Wolff <dylanwolff@gmail.com>
2016-05-09 10:12:55 +0900

    Remove client-side slug uniqueness validation for now

--- apps/admin/lib/admin/posts/validation/form.rb
+++ apps/admin/lib/admin/posts/validation/form.rb
@@ -17,7 +17,7 @@ module Admin
 
         optional(:title).filled
         optional(:body).filled
-        optional(:slug).filled(:slug_unique?)
+        optional(:slug).filled
         optional(:author_id).filled(:int?)
         optional(:status).filled(inclusion?: Entities::Post::Status.values)
       end
