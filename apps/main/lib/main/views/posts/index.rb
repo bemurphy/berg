8ebce2c6bde8d701a87ff4212d69632522f8f9b2
Daniel Nitsikopoulos <daniel.nitsikopoulos@icelab.com.au>
2016-05-09 17:15:01 +1000

    Decorate post with published date

--- apps/main/lib/main/views/posts/index.rb
+++ apps/main/lib/main/views/posts/index.rb
@@ -1,6 +1,7 @@
 require "main/import"
 require "main/paginator"
 require "main/view"
+require "main/decorators/public_post"
 
 module Main
   module Views
@@ -16,8 +17,12 @@ module Main
           options = {per_page: 20, page: 1}.merge(options)
           all_posts = posts.listing(page: options[:page], per_page: options[:per_page])
 
+          public_posts = all_posts.to_a.map { |a|
+            Decorators::PublicPost.new(a)
+          }
+
           super.merge(
-            posts: all_posts.to_a,
+            posts: public_posts,
             paginator: Paginator.new(all_posts.pager, url_template: "/posts?page=%")
           )
         end
