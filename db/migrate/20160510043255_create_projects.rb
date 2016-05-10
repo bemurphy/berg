e4b18d3f234b7e7356cf63a08bcdf3050d067366
Dylan Wolff <dylanwolff@gmail.com>
2016-05-10 13:47:02 +0900

    Create Projects table and relation

---
+++ db/migrate/20160510043255_create_projects.rb
@@ -0,0 +1,30 @@
+ROM::SQL.migration do
+  up do
+    create_table :projects do
+      primary_key :id
+      String :title, null: false
+      String :slug, null: false, unique: true
+      Text :intro, null: false
+      String :url, null: false
+      String :client, null: false
+      Text :body, null: false
+      String :tags, null: false
+      DateTime :created_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+      DateTime :updated_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+    end
+
+    run <<-SQL
+      CREATE TRIGGER set_updated_at_on_projects
+        BEFORE UPDATE ON projects FOR EACH ROW
+        EXECUTE PROCEDURE set_updated_at_column();
+    SQL
+  end
+
+  down do
+    run <<-SQL
+      DROP TRIGGER IF EXISTS set_updated_at_on_projects ON projects;
+    SQL
+
+    drop_table :projects
+  end
+end
---
+++ lib/persistence/relations/projects.rb
@@ -0,0 +1,24 @@
+module Persistence
+  module Relations
+    class Projects < ROM::Relation[:sql]
+      schema(:projects) do
+        attribute :id, Types::Serial
+        attribute :title, Types::String
+        attribute :slug, Types::String
+        attribute :intro, Types::Text
+        attribute :url, Types::String
+        attribute :client, Types::String
+        attribute :body, Types::Text
+        attribute :tags, Types::String
+      end
+
+      def by_id(id)
+        where(id: id)
+      end
+
+      def by_slug(slug)
+        where(slug: slug)
+      end
+    end
+  end
+end
