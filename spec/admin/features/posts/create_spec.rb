a1c4646a1519bbe87ba9123defeeb173e8a086fc
Dylan Wolff <dylanwolff@gmail.com>
2016-05-12 14:35:31 +0900

    Modify Posts create spec

--- spec/admin/features/posts/create_spec.rb
+++ spec/admin/features/posts/create_spec.rb
@@ -7,9 +7,7 @@ RSpec.feature "Admin / Posts / Create", js: true do
     sign_in(jane.email, jane.password)
   end
 
-  # Currently Formalist appears to not add an ID to selection_field field types so we can't
-  # target #author_id - it doesn't exist
-  pending "I can create a post, with a slug generated automatically" do
+  scenario "I can create a post" do
     find("nav a", text: "Posts").trigger("click")
 
     find("a", text: "Add a post").trigger("click")
@@ -17,15 +15,13 @@ RSpec.feature "Admin / Posts / Create", js: true do
     find("#title").set("A sample title")
     find("#teaser").set("A teaser for this sample article")
     find("#body").set("Some sample content for this post")
-    find("#author_id").select("Jane")
+    find("input[name='post[author_id]']", visible: false).set(jane.id)
 
     find("button", text: "Create post").trigger("click")
 
     expect(page).to have_content("Post has been created")
 
     expect(page).to have_content("A sample title")
-
-    expect(page).to have_content("a-sample-title")
   end
 
   scenario "I can see validation errors" do
