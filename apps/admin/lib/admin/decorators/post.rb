0a31e1542894656836717f9b3c9c13bd664ce1fb
Dylan Wolff <dylanwolff@gmail.com>
2016-05-12 10:40:13 +0900

    Move definition of Post status labels into Post decorator

--- apps/admin/lib/admin/decorators/post.rb
+++ apps/admin/lib/admin/decorators/post.rb
@@ -4,6 +4,21 @@ module Admin
       def published_date
         published_at.strftime("%e %b %Y %H:%M:%S%p")
       end
+
+      def status_class
+        case status
+        when "draft"
+          "ghost"
+        when "deleted"
+          "warning"
+        when "published"
+          "success"
+        end
+      end
+
+      def status_label
+        status.capitalize
+      end
     end
   end
 end
