24a3e3331480f97ca5ffce5fa1aa2bce29a0f803
Dylan Wolff <dylanwolff@gmail.com>
2016-05-24 10:43:41 +0900

    Add public Project entity and repository

---
+++ apps/main/lib/main/entities/project.rb
@@ -0,0 +1,28 @@
+require "types"
+
+module Main
+  module Entities
+    class Project < Dry::Types::Value
+      Status = Types::Strict::String.default("draft").enum("draft", "published", "deleted")
+
+      attribute :id, Types::Strict::Int
+      attribute :title, Types::Strict::String
+      attribute :client, Types::Strict::String
+      attribute :url, Types::Strict::String
+      attribute :intro, Types::Strict::String
+      attribute :body, Types::Strict::String
+      attribute :tags, Types::Strict::String
+      attribute :slug, Types::Strict::String
+      attribute :status, Status
+      attribute :published_at, Types::DateTime
+
+      def deleted?
+        status == "deleted"
+      end
+
+      def published?
+        status == "published"
+      end
+    end
+  end
+end
---
+++ apps/main/lib/main/persistence/repositories/projects.rb
@@ -0,0 +1,24 @@
+require "berg/repository"
+require "main/entities/project"
+
+module Main
+  module Persistence
+    module Repositories
+      class Projects < Berg::Repository[:projects]
+        relations :projects
+
+        def by_slug(slug)
+          projects.by_slug(slug).as(Entities::Project).one
+        end
+
+        def listing(per_page: 20, page: 1)
+          projects
+            .per_page(per_page)
+            .page(page)
+            .order(Sequel.desc(:created_at))
+            .as(Entities::Project)
+        end
+      end
+    end
+  end
+end
