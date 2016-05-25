a1a222f3eafff46e3f07974905274dd216b83c76
Dylan Wolff <dylanwolff@gmail.com>
2016-05-26 08:50:09 +0900

    Add a Project entity to the main app

---
+++ apps/main/lib/main/entities/project.rb
@@ -0,0 +1,25 @@
+require "types"
+
+module Main
+  module Entities
+    class Project < Dry::Types::Value
+      Status = Types::Strict::String.default("draft").enum("draft", "published", "deleted")
+
+      attribute :id, Types::Strict::Int
+      attribute :title, Types::Strict::String
+      attribute :client, Types::Strict::String
+      attribute :url, Types::Strict::String
+      attribute :intro, Types::Strict::String
+      attribute :body, Types::String
+      attribute :tags, Types::Strict::String
+      attribute :slug, Types::Strict::String
+      attribute :status, Status
+      attribute :published_at, Types::DateTime
+      attribute :case_study, Types::Bool
+
+      def case_study?
+        case_study == true
+      end
+    end
+  end
+end
